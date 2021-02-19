968ea13e723645a4a28cf3766940773d3ffe57f9
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2021-02-19 21:50:24 +0900

    会員登録フォーム

--- db/migrate/20210217043356_add_name_to_users.rb
+++ db/migrate/20210217043356_add_name_to_users.rb
@@ -1,5 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :nickname, :string
     add_column :users, :family_name, :string
     add_column :users, :first_name, :string
     add_column :users, :family_name_kana, :string
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_043356) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.string "family_name"
     t.string "first_name"
     t.string "family_name_kana"
